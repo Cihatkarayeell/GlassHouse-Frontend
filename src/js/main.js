e31c3aff6ebc72ebb1a52cec1847b96648f55fb3
Cihat <cihatkarayel@icloud.com>
2024-11-30 10:50:35 +0300

    started

---
+++ src/js/main.js
@@ -0,0 +1,29 @@
+const BASE_URL = window.location.protocol + '//' + window.location.host;
+const PATH_URL = window.location.pathname;
+const FULL_URL = window.location.href;
+const app = {};
+const GET_PARAM = (key) => {
+	return new URL(FULL_URL).searchParams.get(key);
+};
+
+
+// PARTIALS
+@@include('partial/header.js') 
+@@include('partial/watermark.js')
+ 
+// UTILS
+@@include('util/fade-out.js')
+@@include('util/smooth-scroll.js')
+@@include('util/replace-broken-image.js')
+
+document.addEventListener('DOMContentLoaded', () => {
+	// PARTIALS
+	@@include('partial/format-tel-link.js')
+	@@include('partial/external-link-norefer.js')
+	@@include('partial/protect-image.js')
+	@@include('partial/customer-sliders.js')
+	/*@@include('partial/responsive-table.js') */
+});
+ 
+
+@@include('partial/init.js') 
